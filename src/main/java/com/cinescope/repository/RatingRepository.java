5268013f94374bd07a0ba676032a0b97d0f6e88c
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-16 19:06:39 +0000

    feat: adicionado RatingRepository para gerenciar a entidade Rating

--- src/main/java/com/cinescope/repository/RatingRepository.java
+++ src/main/java/com/cinescope/repository/RatingRepository.java
@@ -4,8 +4,10 @@ import com.cinescope.model.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
-    // Método personalizado para buscar avaliações por filme
+    // Método personalizado para buscar avaliações por ID do filme
     List<Rating> findByMovieId(Long movieId);
 }
