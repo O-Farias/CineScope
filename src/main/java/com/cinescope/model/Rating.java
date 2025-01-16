ede984f893ce6c23636f2e1457c8f96fbda48c70
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-16 18:17:11 +0000

    Cria entidade Rating
    - Relaciona as entidades User e Movie com ManyToOne
    - Define os atributos: id, score e comment
    - Adiciona mapeamento JPA e métodos utilitários (getters/setters)

---
+++ src/main/java/com/cinescope/model/Rating.java
@@ -0,0 +1,74 @@
+package com.cinescope.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Rating {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private Movie movie;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private int score; // Nota de 0 a 10
+
+    @Column(length = 500)
+    private String comment;
+
+    // Construtores, Getters e Setters
+    public Rating() {}
+
+    public Rating(Movie movie, User user, int score, String comment) {
+        this.movie = movie;
+        this.user = user;
+        this.score = score;
+        this.comment = comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
