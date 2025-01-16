e66ab7777c8b7db5277c134a076db9775efb7bd3
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-16 19:50:26 +0000

    Implementar métodos adicionais no MovieService, incluindo busca por ID

--- src/main/java/com/cinescope/service/MovieService.java
+++ src/main/java/com/cinescope/service/MovieService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -21,6 +22,11 @@ public class MovieService {
     // Busca filmes por gênero
     public List<Movie> getMoviesByGenre(String genre) {
         return movieRepository.findByGenre(genre);
+    }
+
+    // Busca filme por ID
+    public Optional<Movie> getMovieById(Long id) {
+        return movieRepository.findById(id);
     }
 
     // Adiciona um novo filme
